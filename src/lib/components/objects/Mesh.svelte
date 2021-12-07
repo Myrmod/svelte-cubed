52c279c8d0c5bc2ac96519df7c1b5ab0d018e202
Pascal Galle <pascal.galle@gmx.de>
2021-12-07 18:12:15 +0100

    add THREE.Material[] to type of SC.Mesh material

--- src/lib/components/objects/Mesh.svelte
+++ src/lib/components/objects/Mesh.svelte
@@ -7,6 +7,7 @@
 	/** @type {THREE.BufferGeometry} */
 	export let geometry = defaults.geometry;
 
+	/** @type {import('three').Material | import('three').Material[]} */
 	export let material = defaults.material;
 	export let position = defaults.position;
 	export let rotation = defaults.rotation;
